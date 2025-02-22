ca791cd2d82c2bfa4de7a9ede1f9fdc5294d1e8c
Aarushi Ammavajjala <aaru7811@gmail.com>
2025-02-22 13:04:57 -0500

    update

--- stdlib.js
+++ stdlib.js
@@ -8,3 +8,12 @@ export class EaselError extends Error {
         return this.message
     }
 }
+
+export default {
+    ink: args => console.log(...args),
+    random: ([min, max]) => {
+        if (min >= 0 && max <= 1) return Math.random()
+            return Math.random() * (max - min + 1) + min},
+        round: number => Math.round(number)
+    }
+
